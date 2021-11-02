746c2d383989a04719b057db31536f0e5cc7102b
zenodante <yeming@outlook.com>
2021-11-02 15:51:56 +0800

    Create fixstring.h

---
+++ fixstring.h
@@ -0,0 +1,18 @@
+/* Utilities for printing fix16_t datatypes. */
+
+#ifndef _FIXSTRING_H_
+#define _FIXSTRING_H_
+
+#include <stdio.h>
+#include "fix16.h"
+#include "fix16VectMat.h"
+
+
+/* All print_*() functions have interface similar to fprintf().
+ */
+void print_fix16_t(FILE *stream, fix16_t value, uint_fast8_t width, uint_fast8_t decimals);
+void print_qf16(FILE *stream, const qf16 *quat);
+void print_v3d(FILE *stream, const v3d *vector);
+void print_v2d(FILE *stream, const v2d *vector);
+
+#endif
